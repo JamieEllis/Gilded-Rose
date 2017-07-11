bf7224d05d01ab3c32ae0dfceb401958551bcd0c
Jamie Ellis <Jamie.Ellis@softwire.com>
2017-07-11 12:13:42 +0100

    Added functionality to reduce conjured items' quality by double the usual.

--- Gilded Rose/Gilded Rose/GildedRose.cs
+++ Gilded Rose/Gilded Rose/GildedRose.cs
@@ -14,13 +14,16 @@ namespace GildedRose
 		{
 			for (var i = 0; i < Items.Count; i++)
 			{
-				if (Items[i].Name != "Aged Brie" && Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
+			    var itemIsConjured = ((Items[i].Name.Length >= 8) && (Items[i].Name.Substring(0, 8) == "Conjured"));
+			    var conjuredMultiplier = itemIsConjured ? 2 : 1;
+
+                if (Items[i].Name != "Aged Brie" && Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
 				{
 					if (Items[i].Quality > 0)
 					{
 						if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
 						{
-							Items[i].Quality = Items[i].Quality - 1;
+							Items[i].Quality = Items[i].Quality - (1 * conjuredMultiplier);
 						}
 					}
 				}
@@ -36,7 +39,7 @@ namespace GildedRose
 							{
 								if (Items[i].Quality < 50)
 								{
-									Items[i].Quality = Items[i].Quality + 1;
+									Items[i].Quality = Items[i].Quality + (1 * conjuredMultiplier);
 								}
 							}
 							
@@ -44,7 +47,7 @@ namespace GildedRose
 							{
 								if (Items[i].Quality < 50)
 								{
-									Items[i].Quality = Items[i].Quality + 1;
+									Items[i].Quality = Items[i].Quality + (1 * conjuredMultiplier);
 								}
 							}
 						}
@@ -66,7 +69,7 @@ namespace GildedRose
 							{
 								if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
 								{
-									Items[i].Quality = Items[i].Quality - 1;
+									Items[i].Quality = Items[i].Quality - (1 * conjuredMultiplier);
 								}
 							}
 						}
@@ -83,6 +86,11 @@ namespace GildedRose
 						}
 					}
 				}
+
+			    if (Items[i].Quality < 0)
+			    {
+			        Items[i].Quality = 0;
+			    }
 			}
 		}
 		
